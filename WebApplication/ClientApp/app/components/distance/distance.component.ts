f01f944dc9142ad8c1589add9c1d01821d32cf4c
Artur-Laskowski <laskowski.artur.96@gmail.com>
2018-05-23 09:58:28 +0200

    cleaned up distance component

--- WebApplication/ClientApp/app/components/distance/distance.component.ts
+++ WebApplication/ClientApp/app/components/distance/distance.component.ts
@@ -8,33 +8,24 @@ import { Http } from '@angular/http';
 /** distance component*/
 export class DistanceComponent {
     /** distance ctor */
-
-    public httpv: Http;
-    public baseUrlv: string;
+    
     public stations: string[];
     public selectedStartStation: string;
     public selectedEndStation: string;
 
-    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
-        this.httpv = http;
-        this.baseUrlv = baseUrl;
+    constructor(private http: Http, @Inject('BASE_URL') private baseUrl: string) {
 
-        this.httpv.get(this.baseUrlv + 'getAllStations').subscribe(result => {
+        this.http.get(this.baseUrl + 'getAllStations').subscribe(result => {
             this.stations = result.json();
             this.selectedStartStation = this.stations[0];
             this.selectedEndStation = this.stations[0];
         }, error => console.error(error));
     }
-
-    public currentCount = 0;
+    
     public distance = 0;
 
-    public incrementCounter() {
-        this.currentCount++;
-    }
-
-    public getDistance() {
-        this.httpv.get(this.baseUrlv + 'getDistance/?stationA=' + this.selectedStartStation + '&stationB=' + this.selectedEndStation ).subscribe(result => {
+    public getDistance(username: string) {
+        this.http.get(`${this.baseUrl}getDistance/?stationA=${this.selectedStartStation}&stationB=${this.selectedEndStation}` ).subscribe(result => {
             this.distance = result.json();
         }, error => console.error(error));
     }
